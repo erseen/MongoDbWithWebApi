15f493617e007b3ddf88d7eaf46242337c0c1d30
Ersen Yılmaz <ersenjok72@gmail.com>
2023-08-11 20:38:32 +0300

    Crud operations done

---
+++ BookStoreApi/BookStoreApi.Tests/UnitTest1.cs
@@ -0,0 +1,11 @@
+namespace BookStoreApi.Tests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
---
+++ BookStoreApi/BookStoreApi/Models/BookStoreDatabaseSettings.cs
@@ -0,0 +1,9 @@
+﻿namespace BookStoreApi.Models
+{
+    public class BookStoreDatabaseSettings
+    {
+        public string ConnectionString { get; set; } = null!;
+        public string DatabaseName { get; set; } = null!;
+        public string BooksCollectionName { get; set; } = null!;
+    }
+}
